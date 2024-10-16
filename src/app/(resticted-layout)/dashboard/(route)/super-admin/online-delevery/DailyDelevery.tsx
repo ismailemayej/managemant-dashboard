f4c89ead1122150b18d00a97746a7c0f64ad28e6
ismailemayej <salikbd.com@gmai.com>
2024-10-16 22:07:33 +0600

    daily card added

--- src/app/(resticted-layout)/dashboard/(route)/super-admin/online-delevery/DailyDelevery.tsx
+++ src/app/(resticted-layout)/dashboard/(route)/super-admin/online-delevery/DailyDelevery.tsx
@@ -5,7 +5,28 @@ import { columns } from "@/app/(resticted-layout)/components/online-delevery/dai
 
 const DailyDelevery = () => {
   return (
-    <div>
+    <div className="mx-3">
+      <div className="  grid lg:grid-cols-4 justify-center grid-cols-2 gap-4">
+        <div className="lg:w-72 w-64 h-32 rounded-xl bg-slate-100 border pb-6 my-2">
+          <div className="m-4">
+            <h1 className=" text-lg ">Order</h1>
+            <h1 className="text-4xl font-bold mt-2">+6302</h1>
+          </div>
+        </div>
+        <div className="lg:w-72 w-64 h-32 rounded-xl bg-slate-100 border pb-6 my-2">
+          <div className="m-4">
+            <h1 className=" text-lg ">Dispatched Order</h1>
+            <h1 className="text-4xl font-bold mt-2">+8302</h1>
+          </div>
+        </div>
+        <div className="lg:w-72 w-64 h-32 rounded-xl bg-slate-100 border pb-6  lg:my-2">
+          <div className="m-4">
+            <h1 className=" text-lg ">Delivered Order</h1>
+            <h1 className="text-4xl font-bold mt-2">+320</h1>
+          </div>
+        </div>
+      </div>
+
       <DailyDeleveryDataTable columns={columns} data={tasks} />
     </div>
   );
