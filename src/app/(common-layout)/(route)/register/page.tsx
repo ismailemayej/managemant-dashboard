b8f50cc752f3651912f2aa0657a5dba766b33b9f
ismailemayej <salikbd.com@gmai.com>
2024-10-17 22:11:05 +0600

    login and register page done

---
+++ src/app/(common-layout)/(route)/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterPage from "./Register";
+
+const page = () => {
+  return (
+    <div>
+      <RegisterPage />
+    </div>
+  );
+};
+
+export default page;
